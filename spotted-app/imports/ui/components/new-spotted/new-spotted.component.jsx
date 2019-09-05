f8224e46a22c6802a914ddb3c6a6d28c6f7663ec
Pedro Knup <pk@MacBook-Pro-van-Pedro.local>
2019-09-05 23:09:35 +0200

    added color selector

--- spotted-app/imports/ui/components/new-spotted/new-spotted.component.jsx
+++ spotted-app/imports/ui/components/new-spotted/new-spotted.component.jsx
@@ -8,8 +8,28 @@ import { connect } from "react-redux";
 import { withTracker } from "meteor/react-meteor-data";
 import { PAGE_SPOTTED, PAGE_HOME } from "../../redux/constants/pages";
 
+const getRandomColor = () => {
+  const colors = [
+    'yellow',
+    'yellow-orange',
+    'orange',
+    'orange-red',
+    'red',
+    'red-purple',
+    'purple-blue',
+    'purple',
+    'green',
+    'green-yellow',
+    'white',
+    'black'
+  ];
+  const randIndex = Math.floor(Math.random() * (colors.length ));
+  return colors[randIndex];
+};
+
 const NewSpotted = props => {
   const [text, setText] = React.useState("");
+  const [colorClass, setColorClass] = React.useState(getRandomColor());
 
   const openSpottedDetails = () => {
     const spottedPage = PAGE_SPOTTED;
@@ -32,77 +52,11 @@ const NewSpotted = props => {
 
   return (
     <div>
-      <div className="new-spotted spotted-purple white-fg">
-        <svg
-          className="new-spotted-icon new-spotted-more"
-          version="1.1"
-          id="Capa_1"
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          viewBox="0 0 408 408"
-        >
-          <g>
-            <g id="more-horiz">
-              <path
-                d="M51,153c-28.05,0-51,22.95-51,51s22.95,51,51,51s51-22.95,51-51S79.05,153,51,153z M357,153c-28.05,0-51,22.95-51,51
-			s22.95,51,51,51s51-22.95,51-51S385.05,153,357,153z M204,153c-28.05,0-51,22.95-51,51s22.95,51,51,51s51-22.95,51-51
-			S232.05,153,204,153z"
-              />
-            </g>
-          </g>
-        </svg>
-
-        <div className="new-spotted-text">{text}</div>
-
-        <div className="new-spotted-footer">
-          <div className="new-spotted-footer-actions">
-            <span className="new-spotted-likes">
-              <svg
-                className="new-spotted-icon"
-                version="1.1"
-                id="Capa_1"
-                xmlns="http://www.w3.org/2000/svg"
-                x="0px"
-                y="0px"
-                viewBox="0 0 51.997 51.997"
-              >
-                <path
-                  d="M51.911,16.242C51.152,7.888,45.239,1.827,37.839,1.827c-4.93,0-9.444,2.653-11.984,6.905
-                  c-2.517-4.307-6.846-6.906-11.697-6.906c-7.399,0-13.313,6.061-14.071,14.415c-0.06,0.369-0.306,2.311,0.442,5.478
-                  c1.078,4.568,3.568,8.723,7.199,12.013l18.115,16.439l18.426-16.438c3.631-3.291,6.121-7.445,7.199-12.014
-                  C52.216,18.553,51.97,16.611,51.911,16.242z"
-                />
-              </svg>
-            </span>
-            <span className="new-spotted-comments">
-              <svg
-                className="new-spotted-icon"
-                version="1.1"
-                xmlns="http://www.w3.org/2000/svg"
-                x="0px"
-                y="0px"
-                viewBox="0 0 16.087 16.087"
-              >
-                <g>
-                  <g>
-                    <path
-                      d="M6.5,3.478c-3.59,0-6.5,2.238-6.5,5c0,1.354,0.701,2.585,1.839,3.485
-                      c-0.066,0.918-0.287,2.041-0.927,2.646c1.274,0,2.576-0.798,3.399-1.422c0.684,0.188,1.42,0.291,2.189,0.291
-                      c3.59,0,6.5-2.237,6.5-5C13,5.716,10.09,3.478,6.5,3.478z"
-                    />
-                    <path
-                      d="M15.14,8.965C15.687,8.234,16,7.384,16,6.478c0-2.762-2.91-5-6.5-5
-                      c-1.58,0-3.028,0.434-4.154,1.154C5.876,2.532,6.43,2.478,7,2.478c3.866,0,7,2.463,7,5.5c0,1.197-0.485,2.306-1.312,3.207
-                      c0.821,0.624,2.125,1.424,3.399,1.424C15.181,11.749,15.116,9.843,15.14,8.965z"
-                    />
-                  </g>
-                </g>
-              </svg>
-            </span>
-          </div>
-          <span className="new-spotted-distance">source</span>
-        </div>
+      <div
+        className={`new-spotted new-spotted-${colorClass} ${colorClass != "white" &&
+          "white-fg"}`}
+      >
+        <div className="new-spotted-text">{text ? text : "Preview"}</div>
       </div>
       <div className="new-spotted-form">
         <input
@@ -113,31 +67,124 @@ const NewSpotted = props => {
         ></input>
 
         <div className="color-selector">
-          <div className="random">
+          <div
+            onClick={() => {
+              setColorClass(getRandomColor());
+            }}
+            className="random"
+          >
             <div>?</div>
           </div>
-          <div className="red">
+          <div
+            onClick={() => {
+              setColorClass("yellow");
+            }}
+            className="yellow"
+          >
             <div></div>
           </div>
-          <div className="green">
+          <div
+            onClick={() => {
+              setColorClass("yellow-orange");
+            }}
+            className="yellow-orange"
+          >
             <div></div>
           </div>
-          <div className="blue">
+          <div
+            onClick={() => {
+              setColorClass("orange");
+            }}
+            className="orange"
+          >
             <div></div>
           </div>
-          <div className="yellow">
+          <div
+            onClick={() => {
+              setColorClass("orange-red");
+            }}
+            className="orange-red"
+          >
             <div></div>
           </div>
-          <div className="white">
+          <div
+            onClick={() => {
+              setColorClass("red");
+            }}
+            className="red"
+          >
             <div></div>
           </div>
-          <div className="purple">
+          <div
+            onClick={() => {
+              setColorClass("red-purple");
+            }}
+            className="red-purple"
+          >
             <div></div>
           </div>
-          <div className="black">
+          <div
+            onClick={() => {
+              setColorClass("purple");
+            }}
+            className="purple"
+          >
             <div></div>
           </div>
-          <div className="orange">
+          <div
+            onClick={() => {
+              setColorClass("purple-blue");
+            }}
+            className="purple-blue"
+          >
+            <div></div>
+          </div>
+          <div
+            onClick={() => {
+              setColorClass("blue");
+            }}
+            className="blue"
+          >
+            <div></div>
+          </div>
+          <div
+            onClick={() => {
+              setColorClass("blue-green");
+            }}
+            className="blue-green"
+          >
+            <div></div>
+          </div>
+          <div
+            onClick={() => {
+              setColorClass("green");
+            }}
+            className="green"
+          >
+            <div></div>
+          </div>
+          <div
+            onClick={() => {
+              setColorClass("green-yellow");
+            }}
+            className="green-yellow"
+          >
+            <div></div>
+          </div>
+          <div
+            onClick={() => {
+              setColorClass("white");
+            }}
+            className="white"
+          >
+            <div></div>
+          </div>
+          <div
+            onClick={() => {
+              setColorClass("red");
+            }}
+            className="red"
+          >
             <div></div>
           </div>
         </div>
